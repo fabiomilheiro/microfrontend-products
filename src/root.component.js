a88e7f175c7b0d429008e249c380f611862419d3
Fabio Milheiro <fabiomilheiro@gmail.com>
2020-05-21 10:32:54 +0100

    Set navbar placeholder to use id instead of className.

--- src/root.component.js
+++ src/root.component.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Root(props) {
   return (
     <div>
-      <div className="navbar"></div>
+      <div id="navbar"></div>
       <section>{props.name} is mounted!</section>
     </div>
   );
